aca5704e38865c81f0e488f6b2b0d1005e4cfa36
Felipe <f.de.lisboa55@gmail.com>
2023-09-13 13:56:26 -0300

    adicionando mais consolelog

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
 console.log("rodando o sistema de gerenciamento de jogos")
 console.log("aplicação está se conectando ao banco de dados")
+
+//mais um consololog de teste:
+
+console.log("o paulo fez esse commit")
+console.log("enviando para o github")
